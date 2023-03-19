1a6aa57abb75ce2521b8658b0f66ddeef6b86128
Mohau <k.mtheberemohau@gmail.com>
2023-03-19 12:23:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  * Return 1 - (on success)
  */
 int main(void)
-
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout")
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
