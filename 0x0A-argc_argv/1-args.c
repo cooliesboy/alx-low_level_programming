ad7157bf2c215983b1adb58df75f18845ab04201
Mohau <k.mtheberemohau@gmail.com>
2023-04-08 16:25:12 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - prints the number of arguments passed to the program
+ * main - print the number of arguments passed to the program
  * @argc: number of arguments
  * @argv: array of arguments
  * Reutrn: Always 0 (success)
