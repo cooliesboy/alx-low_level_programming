fdcf68acccf91332aaf59840473dbfcf4f342ca5
Mohau <k.mtheberemohau@gmail.com>
2023-04-10 16:47:18 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to be checked
+ * Return: the absolute value of an int
+ */
+int _abs(int n)
+{
+	if (n >= n)
+	{
+		return (n);
+	}
+	return (-n);
+}
